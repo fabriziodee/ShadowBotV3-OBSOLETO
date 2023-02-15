7c4ef3c13f3af199f8625d1358dceacdc2806522
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-15 20:37:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ' , 'https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs' , 'https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F' , 'https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb'] // No tiene utilidad 
 global.channelYT = ['https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'] // No tiene utilidad
-global.owner = ['17722386341'] //Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
-global.mods = ['5219991402134'] //No tiene utilidad
-global.prems = ['5219996125657' , '5219992095479', '972557048356'] //Podra usar algunos comandos del apartado de propietario
+global.owner = ['393518398856'] //Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
+global.mods = ['393518398856'] //No tiene utilidad
+global.prems = ['393518398856' , '393518398856', '393518398856'] //Podra usar algunos comandos del apartado de propietario
 
 //Lo siguiente no tiene nada que ver con el funcionamiento del Bot:
-//Nota: Puedes contactarme si necesitas ayuda con algo al +5219996125657 (este es mi numero personal/real bloqueo personas que no sean temas de la instalación)
-//=> Solo dudas sobre la instalación, temas generales contactarme al +17722386341 o al +5219992095479
-//=> NO ayudo a hacer Bots, ni crearlos, ni editarlos 
-//- Haré tutoriales de como editar el Bot, los videos los podran encontrar en mi canal de YouTube en https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA
+//Nota: puoi contattarmi se hai bisogno di aiuto con qualcosa al +393518398856 (questo è il mio numero personale/reale, blocco le persone che non hanno problemi di installazione)
+//=> Solo dubbi sull'installazione, problemi generali contattatemi al +393518398856
+//=> NON aiuto a creare bot, né a crearli, né a modificarli 
+//- Realizzerò tutorial su come modificare il Bot, i video sono disponibili sul mio canale YouTube all'indirizzo https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA
 
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -56,11 +56,11 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = 'The Shadow Brokers - Bot'
+global.author = 'The Shadow MoonClouds - Bot'
 
 
 global.wm = '©The Shadow Brokers - Bot'
-global.wait = '*Espere un momento..*'
+global.wait = '*Aspetta un secondo..*'
 
 
 global.multiplier = 69 // The higher, The harder levelup
